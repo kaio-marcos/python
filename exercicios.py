e778dc7730917f98c5fa8ac6b199db2a5f480828
kaio <kaiomarcos2015@outlook.com>
2019-07-19 00:15:12 -0300

    exercicios

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+# ---------------------------------- exercicio 1 ---------------------|
+# nome =  input('Qual o nome: ')    
+# horas = float(input('Quantas horas trabalhada parceiro? '))
+# valor = int(input('Por ultimo, quanto vc recebe por hora ?: '))
+# print('pronto, estamos calculando...')
+
+# salario = valor*horas
+# mensal = salario*20
+# print('Você recebe', salario, ' reais por dia....')
+# print('Você recebe', mensal, ' reais mensalmente')
+
+# ---------------------------------- exercicio 2 ---------------------|
